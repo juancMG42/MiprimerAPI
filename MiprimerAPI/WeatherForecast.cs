e09ab18cb686a3a83aa77b96e7e565542f5d0a15
juancMG42 <megamanj804@gmail.com>
2025-11-21 21:58:52 -0500

    se agrega una propiedad según el profesor

--- MiprimerAPI/WeatherForecast.cs
+++ MiprimerAPI/WeatherForecast.cs
@@ -9,5 +9,15 @@ namespace MiprimerAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string city { get; set; }
+
+        public WeatherForecast(DateOnly date, int temperatureC, string? summary, string city)
+        {
+            Date = date;
+            TemperatureC = temperatureC;
+            Summary = summary;
+            this.city = city;
+        }
     }
 }
